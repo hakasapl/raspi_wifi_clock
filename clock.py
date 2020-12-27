0c862e8378f0eae6919c42c385bd5d9d986af234
Hakan Saplakoglu <hakansaplakog@gmail.com>
2020-12-26 19:47:05 -0500

    using hardware clocks

--- clock.py
+++ clock.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as gpio
 import time
 from datetime import datetime
+import pigpio
+import asyncio
 
 class SegmentShifter():
 
@@ -42,29 +44,22 @@ class SegmentShifter():
         gpio.output(self.latchPin, gpio.HIGH)
 
 gpio.setwarnings(False)
-gpio.setmode(gpio.BOARD)
+gpio.setmode(gpio.BCM)
 
-q1_gate = 7
-q2_gate = 29
-q3_gate = 31
-q4_gate = 26
-q5_gate = 24
+gpio_gates = [5,6,7,8]
 
 # shift register pins
-dataPin = 19
-clockPin = 23
-latchPin = 12
+dataPin = 10
+clockPin = 11
+latchPin = 18
+
+pi = pigpio.pi()
 
 shifter = SegmentShifter(dataPin, clockPin, latchPin)
 
-gpio.setup(q1_gate, gpio.OUT)
-gpio.setup(q2_gate, gpio.OUT)
-gpio.setup(q3_gate, gpio.OUT)
-gpio.setup(q4_gate, gpio.OUT)
-gpio.setup(q5_gate, gpio.OUT)
-
-# decimal output is always on
-gpio.output(q1_gate, gpio.HIGH)
+# setup GPIO
+for pin in gpio_gates:
+    gpio.setup(pin, gpio.OUT)
 
 D3D4 = 0b1000000000000000
 D1 = 0b0100000000000000
@@ -78,66 +73,41 @@ C = 0b0000000010000000
 D = 0b0000000001000000
 E = 0b0000000000100000
 
-digit0 = A | B | C | D | E | F
-digit1 = B | C
-digit2 = A | B | G | E | D
-digit3 = A | B | G | C | D
-digit4 = F | G | B | C
-digit5 = A | F | G | C | D
-digit6 = A | F | G | E | D | C
-digit7 = A | B | C
-digit8 = A | B | C | D | E | F | G
-digit9 = A | B | G | F | C | D
+digits = dict()
+digits["0"] = A | B | C | D | E | F
+digits["1"] = B | C
+digits["2"] = A | B | G | E | D
+digits["3"] = A | B | G | C | D
+digits["4"] = F | G | B | C
+digits["5"] = A | F | G | C | D
+digits["6"] = A | F | G | E | D | C
+digits["7"] = A | B | C
+digits["8"] = A | B | C | D | E | F | G
+digits["9"] = A | B | G | F | C | D
 
-def getStream(number):
-    if number == 0:
-        return digit0
-    elif number == 1:
-        return digit1
-    elif number == 2:
-        return digit2
-    elif number == 3:
-        return digit3
-    elif number == 4:
-        return digit4
-    elif number == 5:
-        return digit5
-    elif number == 6:
-        return digit6
-    elif number == 7:
-        return digit7
-    elif number == 8:
-        return digit8
-    elif number == 9:
-        return digit9
 
-def stopDrains():
-    gpio.output(q2_gate, gpio.LOW)
-    gpio.output(q3_gate, gpio.LOW)
-    gpio.output(q4_gate, gpio.LOW)
-    gpio.output(q5_gate, gpio.LOW)
+currentChar = 0
+def nextChar(gpioPin, level, tick):
+    global currentChar
+    global gpio_gates
 
-while True:
-    # get current time
     now = datetime.now()
     ctime = now.strftime("%H%M")
-    
-    for i in range(len(ctime)):
-        stopDrains()
-        shifter.setValue(getStream(int(ctime[i])))
-        if i == 0:
-            # First hour digit
-            gpio.output(q2_gate, gpio.HIGH)
-        elif i == 1:
-            # Second hour digit
-            gpio.output(q3_gate, gpio.HIGH)
-        elif i == 2:
-            # Third hour digit
-            gpio.output(q4_gate, gpio.HIGH)
-        elif i == 3:
-            # Fourth hour digit
-            gpio.output(q5_gate, gpio.HIGH)
-        
-        timer = time.time()
-        while time.time() <= timer + 0.001:
-            continue
+
+    for i in range(len(gpio_gates)):
+        if i != currentChar:
+            gpio.output(gpio_gates[i], gpio.LOW)
+
+    shifter.setValue(digits[ctime[currentChar]])
+    gpio.output(gpio_gates[currentChar], gpio.HIGH)
+
+    currentChar = currentChar + 1
+
+    if currentChar >= 4:
+        currentChar = 0
+
+pi.hardware_clock(4, 5000)
+cb1 = pi.callback(4, pigpio.RISING_EDGE, nextChar)
+
+while True:
+    time.sleep(1)
